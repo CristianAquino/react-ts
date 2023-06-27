ef1b6720ff598686d2ec788985806ed3bf7b516a
cristianaquino <ycristian10@gmail.com>
2023-06-27 11:51:14 -0500

    form_change

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,77 @@
-import { useState } from "react";
+import { ChangeEvent, ReactEventHandler, useState } from "react";
+interface PersonArtwork {
+  title: string;
+  city: string;
+  image: string;
+}
+interface Person {
+  name: string;
+  artwork: PersonArtwork;
+}
+
+const initialState = {
+  name: "Niki de Saint Phalle",
+  artwork: {
+    title: "Blue Nana",
+    city: "Hamburg",
+    image: "https://i.imgur.com/Sd1AgUOm.jpg",
+  },
+};
 
 function App() {
-  const [to, setTo] = useState("Alice");
-  const [message, setMessage] = useState("Hola");
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`sending to: ${to}\n message: ${message}`);
-    }, 5000);
+  const [person, setPerson] = useState<Person>(initialState);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPerson({
+      ...person,
+      artwork: { ...person.artwork, [e.target.name]: e.target.value },
+    });
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <>
       <label>
-        <select value={to} onChange={(e) => setTo(e.target.value)}>
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
-        </select>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={person.name}
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+        />
       </label>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          value={person.artwork.title}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          type="text"
+          name="city"
+          value={person.artwork.city}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          type="text"
+          name="image"
+          value={person.artwork.image}
+          onChange={handleChange}
+        />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i> by {person.name}
+        <br /> (located in {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
   );
 }
 
