1696b6ee3dad852ca45287a8fd2f06a695c291e0
cristianaquino <ycristian10@gmail.com>
2023-06-27 15:56:28 -0500

    feat: check_list

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,62 @@
-import { ChangeEvent, useState } from "react";
-interface PersonArtwork {
+import { useState } from "react";
+
+interface Art {
+  id: number;
   title: string;
-  city: string;
-  image: string;
-}
-interface Person {
-  name: string;
-  artwork: PersonArtwork;
+  seen: boolean;
 }
 
-const initialState = {
-  name: "Niki de Saint Phalle",
-  artwork: {
-    title: "Blue Nana",
-    city: "Hamburg",
-    image: "https://i.imgur.com/Sd1AgUOm.jpg",
-  },
-};
+const initialList = [
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
+];
+
+function ItemList({
+  artworks,
+  onToggle,
+}: {
+  artworks: Art[];
+  onToggle: (id: number) => void;
+}) {
+  return (
+    <ul>
+      {artworks.map((artwork) => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={() => onToggle(artwork.id)}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
-  const [person, setPerson] = useState<Person>(initialState);
+  const [list, setList] = useState<Art[]>(initialList);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPerson({
-      ...person,
-      artwork: { ...person.artwork, [e.target.name]: e.target.value },
+  const handleToggle = (id: number) => {
+    const nuevo = list.map((ele) => {
+      if (ele.id === id) {
+        return {
+          ...ele,
+          seen: !ele.seen,
+        };
+      }
+      return ele;
     });
+    setList(nuevo);
   };
-
   return (
     <>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={person.name}
-          onChange={(e) => setPerson({ ...person, name: e.target.value })}
-        />
-      </label>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="title"
-          value={person.artwork.title}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        City:
-        <input
-          type="text"
-          name="city"
-          value={person.artwork.city}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Image:
-        <input
-          type="text"
-          name="image"
-          value={person.artwork.image}
-          onChange={handleChange}
-        />
-      </label>
-      <p>
-        <i>{person.artwork.title}</i> by {person.name}
-        <br /> (located in {person.artwork.city})
-      </p>
-      <img src={person.artwork.image} alt={person.artwork.title} />
+      <h1>Art Bucket List</h1>
+      <h2>My list of art to see:</h2>
+      <ItemList artworks={list} onToggle={handleToggle} />
     </>
   );
 }
