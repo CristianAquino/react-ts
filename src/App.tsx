3790b6da1da0f5ba76f0cd4d7948fa8060f5756a
cristianaquino <ycristian10@gmail.com>
2023-06-27 11:27:59 -0500

    feat: message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import data from "./data.json";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [to, setTo] = useState("Alice");
+  const [message, setMessage] = useState("Hola");
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`sending to: ${to}\n message: ${message}`);
+    }, 5000);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        <select value={to} onChange={(e) => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default App;
