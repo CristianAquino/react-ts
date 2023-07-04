1731373c0eed2119758a18a0429530c15b386977
cristianaquino <ycristian10@gmail.com>
2023-07-04 11:31:50 -0500

    feat: useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import { useRef } from "react";
+import { useReducer } from "react";
+
+// necesario realizar el tipado de las actions
+interface ActionPerson {
+  type: "increment" | "decrement" | "reset";
+  payload?: any;
+}
+interface InitialPerson {
+  name: string;
+  age: number;
+}
+
+const initialState: InitialPerson = {
+  name: "Jhon",
+  age: 20,
+};
 
 const App = () => {
-  // tipo mas especifico para un tag DIV
-  // si no estamos seguros de que es un DIV, usamos el tipo Element
-  const ref1 = useRef<HTMLDivElement>(null);
+  const reducer = (state: InitialPerson, action: ActionPerson) => {
+    switch (action.type) {
+      case "increment":
+        return { ...state, age: state.age + 1 };
+      case "decrement":
+        return { ...state, age: state.age - 1 };
+      case "reset":
+        return { ...state, age: 20 };
+      default:
+        return state;
+    }
+  };
+
+  const [person, dispatch] = useReducer(reducer, initialState);
   return (
-    <>
-      <div ref={ref1}>Div 1</div>
-      <button
-        onClick={() => {
-          if (ref1.current) ref1.current.style.backgroundColor = "red";
-        }}
-      >
-        Cambiar fondo 1
-      </button>
-    </>
+    <div>
+      <h1>Edad: {person.age}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
   );
 };
 export default App;
