6b2a6efd533fd6f3155ac8a9ea5ad857aeb57234
cristianaquino <ycristian10@gmail.com>
2023-07-05 20:56:44 -0500

    feat: main

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,4 @@
-import { useEffect, useState } from "react";
-import List from "./List";
-
-const initialUser = [
-  { id: 1, name: "Luis" },
-  { id: 2, name: "Maria" },
-];
-
 const App = () => {
-  const [users, setusers] = useState(initialUser);
-  const [text, settext] = useState("");
-
-  const handleAdd = () => {
-    const newUser = { id: users[users.length - 1].id + 1, name: text };
-    setusers([...users, newUser]);
-    settext("");
-  };
-
-  useEffect(() => {
-    console.log("render App");
-  });
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => settext(e.target.value)}
-      />
-      <button onClick={handleAdd}>Add</button>
-      <List users={users} />
-    </div>
-  );
+  return <div>App</div>;
 };
 export default App;
