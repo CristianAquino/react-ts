9168f8c7c4dba986447e5edcd3e03f5c8ea57072
cristianaquino <ycristian10@gmail.com>
2023-07-17 21:41:53 -0500

    feat: redux

---
+++ src/redux/state/user.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface User {
+  user: string;
+  email: string;
+}
+const userEmptyState: User = {
+  user: "",
+  email: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: userEmptyState,
+  reducers: {
+    createUser: (state, action) => {
+      return action.payload;
+    },
+    modifyUser: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+    resetUser: () => {
+      return userEmptyState;
+    },
+  },
+});
+
+export const { createUser, modifyUser, resetUser } = userSlice.actions;
+export default userSlice.reducer;
