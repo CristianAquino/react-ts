560208e61b020daf2465ca274a3fe57368df1bdd
cristianaquino <ycristian10@gmail.com>
2023-07-19 19:24:54 -0500

    feat: base

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { Provider } from "react-redux";
+import "./App.css";
+import { Navbar } from "./components";
+import { Home } from "./pages";
+import store from "./redux/store";
+import { LayoutContainer } from "./styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <LayoutContainer>
+        <Home />
+      </LayoutContainer>
+    </Provider>
+  );
 };
 export default App;
