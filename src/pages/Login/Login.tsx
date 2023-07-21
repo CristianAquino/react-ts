e8cf913887c4b87fde3fa7ac5abd60f72b9da6b5
cristianaquino <ycristian10@gmail.com>
2023-07-21 17:58:15 -0500

    feat: peticion api

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,23 +1,27 @@
 import { useDispatch } from "react-redux";
-import { getMorty } from "../../services";
-import { createUser } from "../../redux/states/user";
 import { useNavigate } from "react-router-dom";
-import { PRIVATE_ROUTES, Roles } from "../../models";
+import { createUserAdapter } from "../../adapters";
+import { useFetchAndLoad } from "../../hooks";
+import { PRIVATE_ROUTES } from "../../models";
+import { createUser } from "../../redux/states/user";
+import { getLogin } from "../../services";
 
 const Login = () => {
+  const { loading, callEndpoint } = useFetchAndLoad();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const login = async () => {
     try {
-      const result = await getMorty();
-      dispatch(createUser({ ...result, rol: Roles.ADMIN }));
+      const result = await callEndpoint(getLogin());
+      dispatch(createUser(createUserAdapter(result)));
       navigate(`/${PRIVATE_ROUTES.PRIVATE}`, { replace: true });
     } catch (error) {}
   };
 
   return (
     <div>
+      {loading && <h2>Loading...</h2>}
       <h2>Hola este es el login</h2>
       <button onClick={login}>Login</button>
     </div>
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,16 @@
+import { loadAbort } from "../helpers";
+import axios from "axios";
+import { UserInfo } from "../models";
+
 const BASE_URL = "https://rickandmortyapi.com/api/";
 const CHARACTER_URL = `${BASE_URL}character/`;
 
-export const getMorty = () => {
-  return fetch(CHARACTER_URL + "2").then((res) => res.json());
+export const getLogin = () => {
+  const controller = loadAbort();
+  return {
+    call: axios.get<UserInfo>(CHARACTER_URL + 2, {
+      signal: controller.signal,
+    }),
+    controller,
+  };
 };
