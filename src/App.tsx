3989fe5fc8d6a5391e28a528ffe5dcb8bd35043f
cristianaquino <ycristian10@gmail.com>
2023-07-31 22:47:53 -0500

    feat: timeAgo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import useTimeAgo from "./utils/timeAgo.utils";
+
 const App = () => {
-  return <div>App</div>;
+  const fecha = "2020-08-07T00:00:00.000Z";
+  const { datetime, timeago } = useTimeAgo(fecha);
+  return (
+    <div>
+      {timeago}"-"{datetime}
+    </div>
+  );
 };
 export default App;
---
+++ src/utils/timeAgo.utils.ts
@@ -0,0 +1,46 @@
+const DATE_UNITS = {
+  day: 86400,
+  hour: 3600,
+  minute: 60,
+  second: 1,
+};
+
+// obteniendo el numero de segundos que han pasado desde un timestamp
+const getSecondsDiff = (timestamp: string): number =>
+  (Date.now() - Date.parse(timestamp)) / 1000;
+const getUnitAndValueData = (secondsElapsed: number) => {
+  for (const [unit, secondsInUnit] of Object.entries(DATE_UNITS)) {
+    if (secondsElapsed >= secondsInUnit || unit === "second") {
+      const value = Math.floor(secondsElapsed / secondsInUnit) * -1;
+      return { value, unit };
+    }
+  }
+  return {
+    value: 0,
+    unit: "",
+  };
+};
+
+const getTimeAgo = (timestamp: string, locale: string) => {
+  const rtf = new Intl.RelativeTimeFormat(locale);
+  const secondsElapsed = getSecondsDiff(timestamp);
+  const { value, unit } = getUnitAndValueData(secondsElapsed);
+  return rtf.format(value, unit);
+};
+
+const useTimeAgo = (timestamp: string) => {
+  const locale = "es";
+  const timeago = getTimeAgo(timestamp, locale);
+  const date = new Date(timestamp);
+  const formattedDate = new Intl.DateTimeFormat(locale, {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }).format(date);
+  return {
+    datetime: formattedDate,
+    timeago,
+  };
+};
+
+export default useTimeAgo;
