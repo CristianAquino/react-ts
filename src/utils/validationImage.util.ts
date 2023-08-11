423a0cfffb8ccee7fb3cf1e7df552bca4f5ba21a
cristianaquino <ycristian10@gmail.com>
2023-08-11 00:54:27 -0500

    feat: validation image

---
+++ src/utils/validationImage.util.ts
@@ -0,0 +1,18 @@
+const fileTypes = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
+
+const validationImage = (img: File) => {
+  return fileTypes.includes(img.type);
+};
+
+export { validationImage, fileTypes };
