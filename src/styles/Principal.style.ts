7ecc3e07105c5fb8acd7fc6ff54fdfa5bf3a78d9
cristianaquino <ycristian10@gmail.com>
2023-08-11 00:51:59 -0500

    style: principal style page

---
+++ src/styles/Principal.style.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Principal = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #262e31;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export { Principal };
