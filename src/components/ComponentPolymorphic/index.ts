1b973de43437f554f87807c7a674552e08636224
cristianaquino <ycristian10@gmail.com>
2023-08-12 10:34:07 -0500

    feat: barrel

---
+++ src/components/ComponentPolymorphic/index.ts
@@ -0,0 +1,1 @@
+export { default as ComponentPolymorphic } from './ComponentPolymorphic';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./ComponentPolymorphic";
 export * from "./PracticeHForm";
 export * from "./ScrollSnap";
 export * from "./StarRating";
