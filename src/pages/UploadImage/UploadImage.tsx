5b6d92ff1094d4ab93afcfbad4facac13eae0d3c
cristianaquino <ycristian10@gmail.com>
2023-08-23 19:48:52 -0500

    feat: refactorizando mediante componentes

--- src/pages/UploadImage/UploadImage.tsx
+++ src/pages/UploadImage/UploadImage.tsx
@@ -1,54 +1,23 @@
-import {
-  AiOutlineCloseCircle,
-  AiOutlineEdit,
-  AiOutlineExpandAlt,
-  AiOutlineSwap,
-} from "react-icons/ai";
+import { AiOutlineSwap } from "react-icons/ai";
 import { UploadImageContext } from "../../context";
-import { Image } from "../../styles";
 import { useCustomerContext } from "../FilterProducts/hooks";
 import {
-  InputUploadImage,
+  ContainerImage,
   FooterUploadImage,
+  InputUploadImage,
   ModalPreviewImage,
   TargetUploadImage,
 } from "./components";
 import { useModal, useUploadImage } from "./hooks";
 import { STORE_UPLOAD_TYPE } from "./reducer";
-import { sharinInformationNameImage } from "./services";
-import {
-  BodyUploadImage,
-  BoxUpluadImage,
-  ButtonIconActions,
-  ButtonsContainerUpload,
-  ImageContainer,
-  LabelTitle,
-  UploadContent,
-} from "./styles";
-import { ChangeEvent } from "react";
+import { BodyUploadImage, LabelTitle, UploadContent } from "./styles";
 export type UploadImageProps = {};
 
 const UploadImage = ({}: UploadImageProps) => {
   const { images, dispatch } = useCustomerContext(UploadImageContext);
   const [isOpen, openModal, closeModal] = useModal();
   const [refImageChane, handleChangeAll] = useUploadImage();
-  const [refImageOnly, , handleChangeOnly] = useUploadImage();
 
-  const handleSharedNameImage = (nameImage: string) => {
-    openModal();
-    sharinInformationNameImage.setObservable(nameImage);
-  };
-  const getMap = () => {
-    if (!refImageOnly.current) {
-      refImageOnly.current = new Map();
-    }
-    return refImageOnly.current;
-  };
-  const handleOnlyImage = (index: number) => {
-    const map = getMap();
-    const node = map?.get(index);
-    node.click();
-  };
   return (
     <UploadContent>
       {isOpen && <ModalPreviewImage closeModal={closeModal} />}
@@ -65,57 +34,7 @@ const UploadImage = ({}: UploadImageProps) => {
       <BodyUploadImage>
         {images.length > 0 ? (
           <>
-            <ImageContainer>
-              {images.map((value, index) => (
-                <BoxUpluadImage
-                  key={value.name}
-                  boxHeight="250px"
-                  boxWidth="100%"
-                  style={{ cursor: "pointer" }}
-                >
-                  <Image
-                    src={URL.createObjectURL(value)}
-                    onClick={() => handleSharedNameImage(value.name)}
-                  />
-                  <ButtonsContainerUpload
-                    style={{ borderRadius: "8px" }}
-                    className="control"
-                  >
-                    <ButtonIconActions
-                      onClick={() => handleSharedNameImage(value.name)}
-                    >
-                      <AiOutlineExpandAlt style={{ color: "#fff" }} />
-                    </ButtonIconActions>
-                    <ButtonIconActions onClick={() => handleOnlyImage(index)}>
-                      <AiOutlineEdit style={{ color: "#248eff" }} />
-                    </ButtonIconActions>
-                    <ButtonIconActions
-                      onClick={() =>
-                        dispatch({
-                          type: STORE_UPLOAD_TYPE.DELETEFILE,
-                          payload: value,
-                        })
-                      }
-                    >
-                      <AiOutlineCloseCircle style={{ color: "#cf1020" }} />
-                    </ButtonIconActions>
-                  </ButtonsContainerUpload>
-                  <InputUploadImage
-                    ref={(node) => {
-                      const map = getMap();
-                      if (node) {
-                        map?.set(index, node);
-                      } else {
-                        map?.delete(index);
-                      }
-                    }}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                      handleChangeOnly(e, index);
-                    }}
-                  />
-                </BoxUpluadImage>
-              ))}
-            </ImageContainer>
+            <ContainerImage images={images} openModal={openModal} />
             <FooterUploadImage
               onReset={() => dispatch({ type: STORE_UPLOAD_TYPE.RESET })}
               onAdd={() => refImageChane.current?.click()}
