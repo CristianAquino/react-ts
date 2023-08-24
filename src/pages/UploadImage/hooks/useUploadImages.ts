3b876e9d07772a93befcc53159315c0703af9e04
cristianaquino <ycristian10@gmail.com>
2023-08-23 21:05:04 -0500

    feat: validate images

--- src/pages/UploadImage/hooks/useUploadImages.ts
+++ src/pages/UploadImage/hooks/useUploadImages.ts
@@ -2,6 +2,7 @@ import { ChangeEvent, useRef } from "react";
 import { useCustomerContext } from "../../FilterProducts/hooks";
 import { UploadImageContext } from "../../../context";
 import { STORE_UPLOAD_TYPE } from "../reducer";
+import { validationImage, notifyError } from "../utils";
 
 export const useUploadImage = () => {
   const imgRef = useRef<any>(null);
@@ -9,17 +10,34 @@ export const useUploadImage = () => {
   const { dispatch } = useCustomerContext(UploadImageContext);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!(e.target.files && e.target.files.length > 0)) return;
+    let cont = 0;
     const files = e.target.files;
-    dispatch({ type: STORE_UPLOAD_TYPE.ADDFILES, payload: files });
+    for (let file of files) {
+      if (validationImage(file)) {
+        dispatch({ type: STORE_UPLOAD_TYPE.ADDFILES, payload: file });
+      } else {
+        cont++;
+      }
+    }
+    if (cont !== 0) {
+      notifyError("SE ENCONTRARON ELEMENTOS QUE NO SON VALIDOS");
+    }
   };
-
   const handleEdit = (e: ChangeEvent<HTMLInputElement>, index: number) => {
     if (!(e.target.files && e.target.files[0])) return;
+    let cont = 0;
     const file = e.target.files[0];
-    dispatch({
-      type: STORE_UPLOAD_TYPE.EDITIMAGE,
-      payload: { image: file, index },
-    });
+    if (validationImage(file)) {
+      dispatch({
+        type: STORE_UPLOAD_TYPE.EDITIMAGE,
+        payload: { image: file, index },
+      });
+    } else {
+      cont++;
+    }
+    if (cont !== 0) {
+      notifyError("ELEMENTO NO VALIDO");
+    }
   };
   return [imgRef, handleChange, handleEdit] as const;
 };
