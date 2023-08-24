f991a9b154cbf670ff520e4b67ca33759cd10e28
cristianaquino <ycristian10@gmail.com>
2023-08-23 19:51:01 -0500

    feat: barrel

---
+++ src/pages/UploadImage/components/ImageContentBox/index.ts
@@ -0,0 +1,1 @@
+export { default as ImageContentBox } from './ImageContentBox';
--- src/pages/UploadImage/components/index.ts
+++ src/pages/UploadImage/components/index.ts
@@ -1,4 +1,6 @@
+export * from "./ContainerImage";
 export * from "./FooterUploadImage";
+export * from "./ImageContentBox";
 export * from "./InputUploadImage";
 export * from "./ModalPreviewImage";
 export * from "./TargetUploadImage";
