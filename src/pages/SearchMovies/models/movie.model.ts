1b75d5cfd1ef00cf3d56948d6b03b8616a802ad7
cristianaquino <ycristian10@gmail.com>
2023-08-28 20:56:39 -0500

    feat: movies model

---
+++ src/pages/SearchMovies/models/movie.model.ts
@@ -0,0 +1,14 @@
+export interface EndpointMovie {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+export interface Movie {
+  title: string;
+  year: string;
+  id: string;
+  type: string;
+  poster: string;
+}
