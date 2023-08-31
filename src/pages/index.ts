5291a8299261493a6f12cb783f19696827c91610
cristianaquino <ycristian10@gmail.com>
2023-08-30 22:06:07 -0500

    feat: barrel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,6 @@
 export * from "./FilterProducts";
 export * from "./UploadImage";
 export * from "./CatFact";
+export * from "./WebCamAccess";
+export * from "./SearchMovies";
+export * from "./PracticeHooks";
